b1f6d7d7d6cd2a8404bf21046c42bdb852b83f8e
anushkachouhan029 <anushkachouhan029>
2023-01-06 21:05:33 +0530

    added new project

---
+++ ensure that the player gets the winning position 100/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Game !!");
+
+            const int StartPosition = 0;
+            const int WinningPosition = 100;
+            int PlayerPosition = 0;
+            int diceRoll;
+
+
+            Console.WriteLine("\nPress ENTER to Play the game");
+            string play = Console.ReadLine();
+            Console.WriteLine($"Player is at position {PlayerPosition}");
+
+            Random random = new Random();
+            while (PlayerPosition < WinningPosition)
+            {
+                diceRoll = random.Next(1, 7);
+                Console.WriteLine($"\nPlayer rolled number {diceRoll} on position {PlayerPosition}");
+
+                int option = random.Next(1, 3);
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Player landed on a Ladder");
+                        PlayerPosition += diceRoll;
+                        Console.WriteLine($"Player moves to position {PlayerPosition}");
+                        if (PlayerPosition > WinningPosition)
+                        {
+                            PlayerPosition = PlayerPosition - diceRoll;
+                            Console.WriteLine($"Player remains in position {PlayerPosition}");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Player landed on a Snake");
+                        PlayerPosition -= diceRoll;
+                        Console.WriteLine($"Player moves to position {PlayerPosition}");
+                        if (PlayerPosition < 0)
+                        {
+                            Console.WriteLine("No Play, Try Again");
+                            PlayerPosition = 0;
+                        }
+                        break;
+                }
+            }
+            if (PlayerPosition == 100)
+            {
+                Console.WriteLine("==========Player Wins==========");
+            }
+        }
+    }
+}
